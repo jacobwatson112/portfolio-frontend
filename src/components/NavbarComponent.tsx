3998d76c9312de272984a619862fcf7fcf5b01db
Jacob <jacobwatson112@gmail.com>
2022-10-20 12:40:27 +1300

    Buttons float to right of Navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Button, Flex, IconButton, Spacer, Text } from "@chakra-ui/react";
 import { AiOutlineHome, AiOutlineMail, AiOutlineBulb } from 'react-icons/ai';
 import { useNavigate } from "react-router-dom";
 
@@ -7,9 +7,15 @@ function NavbarComponent() {
   return (
     <>
       <Flex h="50px" bg="green" w='100%'>
-        <Button h="100%" onClick={() => navigate('/')} leftIcon={<AiOutlineHome size={25} />}>Home</Button>
-        <Button h="100%" leftIcon={<AiOutlineBulb size={25} />}>Projects</Button>
-        <Button h="100%" leftIcon={<AiOutlineMail size={25} />}>Contact</Button>
+        <Box>
+          <Text color='black' fontWeight='bold' fontSize='32px' ml='50px' mr='50px'>Jacob Watson</Text>
+        </Box>
+        <Spacer />
+        <Box bg='blue' alignContent='right' h='100%'>
+          <Button h="100%" ml='10px' mr='10px' onClick={() => navigate('/')} leftIcon={<AiOutlineHome size={25} />}>Home</Button>
+          <Button h="100%" ml='10px' mr='10px' leftIcon={<AiOutlineBulb size={25} />}>Projects</Button>
+          <Button h="100%" ml='10px' mr='10px' leftIcon={<AiOutlineMail size={25} />}>Contact</Button>
+        </Box>
       </Flex>
     </>
   );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavbarComponent from "../components/NavbarComponent";
 
 function HomeView(){
+    const [windowDimenion, detectHW] = useState({
+        winWidth: window.innerWidth,
+        winHeight: window.innerHeight,
+      })
+    
+      const detectSize = () => {
+        detectHW({
+          winWidth: window.innerWidth,
+          winHeight: window.innerHeight,
+        })
+      }
+    
+      useEffect(() => {
+        window.addEventListener('resize', detectSize)
+    
+        return () => {
+          window.removeEventListener('resize', detectSize)
+        }
+      }, [windowDimenion])
+      
     return(
         <>
         <NavbarComponent></NavbarComponent>
         <h1>Pog</h1>
+
+        <p>Width: <strong>{windowDimenion.winWidth}</strong></p>
+        <p>Height: <strong>{windowDimenion.winHeight}</strong></p>
         
         </>
     )
